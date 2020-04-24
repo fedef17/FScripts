a96f10568d96e7cd09619c07d0760c79b1644d11
fedef17 <frithurik88@gmail.com>
2020-04-24 19:45:28 +0200

    added final figure delta+trend

--- cmip6_WRfreq_v4.py
+++ cmip6_WRfreq_v4.py
@@ -571,9 +571,8 @@ for area in ['EAT', 'PNA']:
         allpercs['ens_max'] = [np.percentile(freqs[(ssp, 'all', 'last20')][:, reg], 75) for ssp in allsims]
 
         ax.axhline(allpercs['p50'][0], color = 'gray', linewidth = 0.5)
-        print('AAAAAAAAAAAAAAAAAAAAAAAAAAA')
-        print(allsims)
-        ctl.boxplot_on_ax(ax, allpercs, allsims, colsim, plot_mean = True, plot_ensmeans = True, plot_minmax = True)
+
+        ctl.boxplot_on_ax(ax, allpercs, allsims, colsim, plot_mean = True, plot_ensmeans = False, plot_minmax = True)
         ax.set_xticks([])
         ax.set_title(reg_names[reg])
 
