2921a643e73af624660b745bf1c72f60ea589065
fedef17 <frithurik88@gmail.com>
2020-05-23 23:27:09 +0200

    geostrophy exe

---
+++ cmip6_trend_from_pcs.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import sys
+import os
+from matplotlib import pyplot as plt
+from matplotlib import cm
+
+import pickle
+import netCDF4 as nc
+
+import climtools_lib as ctl
+import climdiags as cd
+
+from matplotlib.colors import LogNorm
+from datetime import datetime
+
+from scipy import stats
+import pandas as pd
+
+#############################################################################
+if os.uname()[1] == 'hobbes':
+    cart_in = '/home/fabiano/Research/lavori/CMIP6/'
+elif os.uname()[1] == 'ff-clevo':
+    cart_in = '/home/fedefab/Scrivania/Research/Post-doc/lavori/CMIP6/'
+
+cart_out_orig = cart_in + 'Results_trendssp585/'
+ctl.mkdir(cart_out_orig)
+
+file_hist = cart_in + 'cmip6_hist/out_cmip6_hist_NDJFM_{}_4clus_4pcs_1964-2014_refCLUS_dtr.p'
+#file_hist_refEOF = cart_in + 'cmip6_hist/out_cmip6_hist_NDJFM_EAT_4clus_4pcs_1964-2014_refEOF.p'
+gen_file_ssp = cart_in + 'cmip6_{}/out_cmip6_{}_NDJFM_{}_4clus_4pcs_2015-2100_refCLUS_dtr.p'
+
+numclus = 4
+reg_names_area = dict()
+reg_names_area['EAT'] = ['NAO+', 'SBL', 'AR', 'NAO-']
+reg_names_area['PNA'] = ['PT', 'PNA+', 'PNA-', 'AR']
+
+
+area = 'EAT'
+ssp = 'ssp585'
+
+results_hist, results_ref = pickle.load(open(file_hist.format(area), 'rb'))
+results_ssp = pickle.load(open(gen_file_ssp.format(ssp, ssp, area), 'rb'))['models']
+
+cart_lui = cart_in + 'Results_v4/{}_NDJFM/'.format(area)
+freqs, residtimes = pickle.load(open(cart_lui + 'allresults_dicts_{}_v3.p'.format(area), 'rb'))
+
+okmods = [ke[1] for ke in freqs if 'ssp585' in ke and 'tot50' in ke and 'all' not in ke and 'rel' not in ke]
+#['BCC-CSM2-MR_r1i1p1f1', 'CanESM5_r1i1p1f1', 'CESM2-WACCM_r1i1p1f1\', 'CNRM-CM6-1_r1i1p1f2', 'CNRM-ESM2-1_r1i1p1f2', 'EC-Earth3_r1i1p1f1', 'FGOALS-g3_r1i1p1f1', 'INM-CM4-8_r1i1p1f1', 'INM-CM5-0_r1i1p1f1', 'IPSL-CM6A-LR_r1i1p1f1', 'MIROC6_r1i1p1f1', 'MPI-ESM1-2-HR_r1i1p1f1', 'MRI-ESM2-0_r1i1p1f1', 'UKESM1-0-LL_r1i1p1f2']
+
+for nu in range(4):
+    for mod in okmods:
+        var, dat = ctl.seasonal_set(results_ssp['EC-Earth3_r1i1p1f1']['pcs'][:, 0], results_ssp['EC-Earth3_r1i1p1f1']['dates'], 'DJF', seasonal_average = True)
+        years = np.array([da.year for da in dat])
+        m, c, err_m, err_c = ctl.linear_regre_witherr(years, var)
+
+        daynum = np.arange(len(results_ssp['EC-Earth3_r1i1p1f1']['dates']))
+        m, c, err_m, err_c = ctl.linear_regre_witherr(daynum, results_ssp['EC-Earth3_r1i1p1f1']['pcs'])
+
+cartmon_hist = '/data-hobbes/fabiano/CMIP6/historical_mon_zg/'
+cartmon_ssp = '/data-hobbes/fabiano/CMIP6/ssp585_mon_zg/'
+
+filssp = 'zg_Amon_ssp585_{}_{}_2015-2100.nc'
+filhist = 'zg_{}_mon.nc'
---
+++ geostrophy_exe.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import sys
+import os
+from matplotlib import pyplot as plt
+from matplotlib import cm
+
+import pickle
+import netCDF4 as nc
+
+import climtools_lib as ctl
+import climdiags as cd
+
+from matplotlib.colors import LogNorm
+from datetime import datetime
+
+from scipy import stats
+import pandas as pd
+
+#############################
+
+cart_in = '/nas/reference/ERAInterim/daily/'
+filnam = cart_in + '{}/{}_Aday_ERAInterim_201801_201812.nc'
+
+cart_out = '/home/fabiano/Research/lavori/dynamics_exe/'
+
+# 29 Dec 2018
+u, ucoords, _ = ctl.readxDncfield(filnam.format('u', 'u'))
+u = u[-3, ...]
+v, vcoords, _ = ctl.readxDncfield(filnam.format('v', 'v'))
+v = v[-3, ...]
+ta, tacoords, _ = ctl.readxDncfield(filnam.format('ta', 'ta'))
+ta = ta[-3, ...]
+zg, zgcoords, _ = ctl.readxDncfield(filnam.format('zg', 'zg'))
+zg = zg[-3, ...]
+pv, pvcoords, _ = ctl.readxDncfield(filnam.format('pv', 'pv'))
+pv = pv[-3, ...]
+
+#u.shape (365, 5, 241, 480)
+lat = ucoords['lat']
+lon = ucoords['lon']
+
+Om = 2*np.pi/86400.
+f = 2*Om*np.sin(np.deg2rad(lat))
+
+levs = np.array([850, 500, 200, 50, 30])
+
+grad_zg = dict()
+ug = dict()
+vg = dict()
+vortg = dict()
+grad_ta = dict()
+
+for i, lev in enumerate(levs):
+    grad_zg[lev] = ctl.calc_gradient_2d(zg[i], lat, lon)
+    ug[lev] = -1/f * grad_zg[lev][1]
+    vg[lev] = 1/f * grad_zg[lev][0]
+
+    laplzg = ctl.calc_gradient_2d(grad_zg[lev][0], lat, lon)[0] + ctl.calc_gradient_2d(grad_zg[lev][1], lat, lon)[1]
+    vortg[lev] = 1/f * laplzg
+
+    grad_ta[lev] = ctl.calc_gradient_2d(ta[i], lat, lon)
+
+R = 8.31
+ut = np.sum([ R/f * np.log(lev1/lev2) * grad_ta[lev][1] for lev1, lev2 in zip(levs[:-1], levs[1:])])
+vt = np.sum([-R/f * np.log(lev1/lev2) * grad_ta[lev][0] for lev1, lev2 in zip(levs[:-1], levs[1:])])
+
+tam = np.mean(ta, axis = 0)
+
+figs = []
+for i, lev in levs:
+    fig = ctl.plot_map_contour(zg[i], lat, lon, add_vector_field = [u[i], v[i]], title = 'real winds - lev {} hPa'.format(lev), plot_anomalies = False)
+    figs.append(fig)
+    fig = ctl.plot_map_contour(zg[i], lat, lon, add_vector_field = [ug[lev], vg[lev]], title = 'geostrophic winds - lev {} hPa'.format(lev), plot_anomalies = False)
+    figs.append(fig)
+    fig = ctl.plot_map_contour(zg[i], lat, lon, add_vector_field = [u[i] - ug[lev], v[i] - vg[lev]], title = 'ageostrophic winds - lev {} hPa'.format(lev), plot_anomalies = False)
+    figs.append(fig)
+    fig = ctl.plot_map_contour(vortg[lev], lat, lon, add_contour_field = pv[i], title = 'geostrophic vort and PV - lev {} hPa'.format(lev), plot_anomalies = False)
+    figs.append(fig)
+
+fig = ctl.plot_map_contour(tam, lat, lon, add_vector_field = [ut, vt], title = 'temperature and thermal wind', plot_anomalies = False)
+figs.append(fig)
+
+fig = ctl.plot_map_contour(tam, lat, lon, add_vector_field = [u[-1]-ug[levs[0]]-ut, v[-1]-vg[levs[0]]-vt], title = 'wind at height minus thermal wind', plot_anomalies = False)
+figs.append(fig)
+
+ctl.plot_pdfpages(cart_out + 'geostrophy_exe.pdf', figs)
