032bae2160d7104e581bf02c6de9c552f2286579
fedef17 <frithurik88@gmail.com>
2020-05-27 18:05:44 +0200

    cmip6 trend

--- geostrophy_exe.py
+++ geostrophy_exe.py
@@ -90,7 +90,7 @@ for i, lev in enumerate(levs):
     figs.append(fig)
     fig = ctl.plot_map_contour(zg_[i], lat, lon, add_vector_field = [u_[i] - ug[lev], v_[i] - vg[lev]], title = 'ageostrophic winds - lev {} hPa'.format(lev), plot_anomalies = False, plot_margins = area, quiver_scale = quiver_scale, vec_every = vec_every)
     figs.append(fig)
-    fig = ctl.plot_map_contour(vortg[lev], lat, lon, add_contour_field = pv_[i], title = 'geostrophic vort and PV - lev {} hPa'.format(lev), plot_anomalies = False, plot_margins = area, vec_every = vec_every)
+    fig = ctl.plot_map_contour(vortg[lev], lat, lon, add_contour_field = pv_[i], title = 'geostrophic vort and PV - lev {} hPa'.format(lev), plot_anomalies = False, plot_margins = area, vec_every = vec_every, plot_type = 'pcolormesh')
     figs.append(fig)
 
 fig = ctl.plot_map_contour(tam, lat, lon, add_vector_field = [ut, vt], title = 'temperature and thermal wind', plot_anomalies = False, plot_margins = area, quiver_scale = quiver_scale, vec_every = vec_every)
