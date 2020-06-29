46f2ba4129f300e004dd61c94aa748ebc6c0ca55
fedef17 <frithurik88@gmail.com>
2020-06-29 14:04:36 +0200

    cmip6 rebase

--- cmip6_histssp_reground.py
+++ cmip6_histssp_reground.py
@@ -30,6 +30,8 @@ gen_file_ssp = cart_in + 'out_NEW_cmip6_{}_NDJFM_{}_4clus_4pcs_2015-2100_refCLUS
 file_rebase = cart_in + 'out_NEW_cmip6_{}_NDJFM_{}_4clus_4pcs_2015-2100_refCLUS_dtr_histrebase.p'
 file_light = cart_in + 'out_NEW_cmip6_{}_NDJFM_{}_4clus_4pcs_2015-2100_refCLUS_dtr_light.p'
 
+filref = '/home/fabiano/Research/lavori/WeatherRegimes/ERA_ref_r25_v4/out_ERA_NDJFM_{}_4clus_4pcs_1964-2014_dtr.p'
+
 numclus = 4
 reg_names_area = dict()
 reg_names_area['EAT'] = ['NAO+', 'SBL', 'AR', 'NAO-']
@@ -39,6 +41,7 @@ area = 'EAT'
 allssps = 'ssp126 ssp245 ssp370 ssp585'.split()
 for area in ['EAT']:#, 'PNA']:
     results_hist, results_ref = ctl.load_wrtool(file_hist.format(area))
+    results_ref = pickle.load(open(filref.format(area), 'rb'))
     # del results_ref['var_glob']
     # del results_ref['var_area']
     # del results_ref['solver']
