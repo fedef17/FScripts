524909b487c5af32bf2086fd9cee637f1d3e3c9d
fedef17 <frithurik88@gmail.com>
2020-07-23 18:41:07 +0200

    pretreat cdo

--- pretreat_cdo_wilma.py
+++ pretreat_cdo_wilma.py
@@ -21,89 +21,94 @@ import pandas as pd
 
 #################################
 
-# cart_orig = '/archive/paolo/cmip5/CMIP5/output1/'
-# listacarts = glob.glob(cart_orig + '*/*/rcp85/mon/atmos/Amon/r1i1*/ta/')
-# ssp = 'rcp85'
-cart_orig = '/archive/paolo/cmip6/CMIP6/model-output/'
-listacarts = glob.glob(cart_orig + '*/*/ssp585/atmos/Amon/r1i1*/ta/')
-ssp = 'ssp585'
-
-cartou = '/home/federico/work/CMIP6/data_mon_ta/'
-cart_g_ut = cartou + 'UTmean/'
-ctl.mkdir(cart_g_ut)
-cart_g_lt = cartou + 'LTmean/'
-ctl.mkdir(cart_g_lt)
-cart_g_s = cartou + 'Smean/'
-ctl.mkdir(cart_g_s)
-
-for cart in listacarts:
-    mod = cart.split('/')[7]
-    # mem = cart.split('/')[12]
-    mem = cart.split('/')[11]
-    print('----------------------------\n')
-    print(mod, mem)
-    print('----------------------------\n')
-
-    cartut = cart_g_ut + '{}_{}_{}/'.format(mod, mem, ssp)
-    ctl.mkdir(cartut)
-    cartstrat = cart_g_s + '{}_{}_{}/'.format(mod, mem, ssp)
-    ctl.mkdir(cartstrat)
-    cartlt = cart_g_lt + '{}_{}_{}/'.format(mod, mem, ssp)
-    ctl.mkdir(cartlt)
-
-    file_list = [co.split('/')[-1] for co in glob.glob(cart+'ta*nc')]
-
-    for filenam in file_list:
-        if float(filenam.split('_')[-1][:-3].split('-')[1][:4]) <= 2100:
-            pass
-        else:
-            continue
-
-        file_in = cart + filenam
-        indpo = filenam.index('.nc')
-
-        filepart = filenam[:indpo] + '_UTmean.nc'
-        file_out = cartut + filepart
-        command = 'cdo sellevel,40000,30000,25000,20000,15000 {} prov.nc'.format(file_in)
-        os.system(command)
-        command = 'cdo vertmean prov.nc {}'.format(file_out)
-        os.system(command)
-
-        filepart = filenam[:indpo] + '_Smean.nc'
-        file_out = cartstrat + filepart
-        command = 'cdo sellevel,25000,20000,15000,10000,7000,5000,3000 {} prov.nc'.format(file_in)
-        os.system(command)
-        command = 'cdo vertmean prov.nc {}'.format(file_out)
-        os.system(command)
-
-        filepart = filenam[:indpo] + '_LTmean.nc'
-        file_out = cartlt + filepart
-        command = 'cdo sellevel,100000,92500,85000,70000 {} prov.nc'.format(file_in)
-        os.system(command)
-        command = 'cdo vertmean prov.nc {}'.format(file_out)
-        os.system(command)
-
-    if len(file_list) > 1:
-        command = 'cdo cat {}*UTmean.nc {}ta_{}_{}_2015-2100_UTmean.nc'.format(cartut, cartut, mod, mem)
-        os.system(command)
-        command = 'cdo cat {}*Smean.nc {}ta_{}_{}_2015-2100_Smean.nc'.format(cartstrat, cartstrat, mod, mem)
-        os.system(command)
-        command = 'cdo cat {}*LTmean.nc {}ta_{}_{}_2015-2100_LTmean.nc'.format(cartlt, cartlt, mod, mem)
-        os.system(command)
+for ssp in ['ssp585', 'rcp85']:
+    # ssp = 'rcp85'
+    if ssp == 'rcp85':
+        cart_orig = '/archive/paolo/cmip5/CMIP5/output1/'
+        listacarts = glob.glob(cart_orig + '*/*/rcp85/mon/atmos/Amon/r1i1*/ta/')
     else:
-        command = 'cp {}*UTmean.nc {}ta_{}_{}_2015-2100_UTmean.nc'.format(cartut, cartut, mod, mem)
-        os.system(command)
-        command = 'cp {}*Smean.nc {}ta_{}_{}_2015-2100_Smean.nc'.format(cartstrat, cartstrat, mod, mem)
-        os.system(command)
-        command = 'cp {}*LTmean.nc {}ta_{}_{}_2015-2100_LTmean.nc'.format(cartlt, cartlt, mod, mem)
+        cart_orig = '/archive/paolo/cmip6/CMIP6/model-output/'
+        listacarts = glob.glob(cart_orig + '*/*/ssp585/atmos/Amon/r1i1*/ta/')
+
+    cartou = '/home/federico/work/CMIP6/data_mon_ta/'
+    cart_g_ut = cartou + 'UTmean/'
+    ctl.mkdir(cart_g_ut)
+    cart_g_lt = cartou + 'LTmean/'
+    ctl.mkdir(cart_g_lt)
+    cart_g_s = cartou + 'Smean/'
+    ctl.mkdir(cart_g_s)
+
+    for cart in listacarts:
+        mod = cart.split('/')[7]
+        if ssp == 'rcp85':
+            mem = cart.split('/')[12]
+        else:
+            mem = cart.split('/')[11]
+
+        print('----------------------------\n')
+        print(mod, mem)
+        print('----------------------------\n')
+
+        cartut = cart_g_ut + '{}_{}_{}/'.format(mod, mem, ssp)
+        ctl.mkdir(cartut)
+        cartstrat = cart_g_s + '{}_{}_{}/'.format(mod, mem, ssp)
+        ctl.mkdir(cartstrat)
+        cartlt = cart_g_lt + '{}_{}_{}/'.format(mod, mem, ssp)
+        ctl.mkdir(cartlt)
+
+        file_list = [co.split('/')[-1] for co in glob.glob(cart+'ta*nc')]
+
+        for filenam in file_list:
+            if float(filenam.split('_')[-1][:-3].split('-')[1][:4]) <= 2100:
+                pass
+            else:
+                continue
+
+            file_in = cart + filenam
+            indpo = filenam.index('.nc')
+
+            filepart = filenam[:indpo] + '_UTmean.nc'
+            file_out = cartut + filepart
+            command = 'cdo sellevel,40000,30000,25000,20000,15000 {} {}prov.nc'.format(file_in, cartut)
+            os.system(command)
+            command = 'cdo vertmean {}prov.nc {}'.format(cartut, file_out)
+            os.system(command)
+
+            filepart = filenam[:indpo] + '_Smean.nc'
+            file_out = cartstrat + filepart
+            command = 'cdo sellevel,25000,20000,15000,10000,7000,5000,3000 {} {}prov.nc'.format(file_in, cartstrat)
+            os.system(command)
+            command = 'cdo vertmean {}prov.nc {}'.format(cartstrat, file_out)
+            os.system(command)
+
+            filepart = filenam[:indpo] + '_LTmean.nc'
+            file_out = cartlt + filepart
+            command = 'cdo sellevel,100000,92500,85000,70000 {} {}prov.nc'.format(file_in, cartlt)
+            os.system(command)
+            command = 'cdo vertmean {}prov.nc {}'.format(cartlt, file_out)
+            os.system(command)
+
+        if len(file_list) > 1:
+            command = 'cdo cat {}*UTmean.nc {}ta_{}_{}_2015-2100_UTmean.nc'.format(cartut, cartut, mod, mem)
+            os.system(command)
+            command = 'cdo cat {}*Smean.nc {}ta_{}_{}_2015-2100_Smean.nc'.format(cartstrat, cartstrat, mod, mem)
+            os.system(command)
+            command = 'cdo cat {}*LTmean.nc {}ta_{}_{}_2015-2100_LTmean.nc'.format(cartlt, cartlt, mod, mem)
+            os.system(command)
+        else:
+            command = 'cp {}*UTmean.nc {}ta_{}_{}_2015-2100_UTmean.nc'.format(cartut, cartut, mod, mem)
+            os.system(command)
+            command = 'cp {}*Smean.nc {}ta_{}_{}_2015-2100_Smean.nc'.format(cartstrat, cartstrat, mod, mem)
+            os.system(command)
+            command = 'cp {}*LTmean.nc {}ta_{}_{}_2015-2100_LTmean.nc'.format(cartlt, cartlt, mod, mem)
+            os.system(command)
+
+        # regrid
+        command = 'cdo remapbil,r360x180 {}ta_{}_{}_2015-2100_UTmean.nc {}ta_{}_{}_{}_2015-2100_UTmean_r1.nc'.format(cartut, mod, mem, cart_g_ut, mod, mem, ssp)
         os.system(command)
 
-    # regrid
-    command = 'cdo remapbil,r360x180 {}ta_{}_{}_2015-2100_UTmean.nc {}ta_{}_{}_2015-2100_UTmean_r1.nc'.format(cartut, mod, mem, cart_g_ut, mod, mem)
-    os.system(command)
+        command = 'cdo remapbil,r360x180 {}ta_{}_{}_2015-2100_Smean.nc {}ta_{}_{}_{}_2015-2100_Smean_r1.nc'.format(cartstrat, mod, mem, cart_g_s, mod, mem, ssp)
+        os.system(command)
 
-    command = 'cdo remapbil,r360x180 {}ta_{}_{}_2015-2100_Smean.nc {}ta_{}_{}_2015-2100_Smean_r1.nc'.format(cartstrat, mod, mem, cart_g_s, mod, mem)
-    os.system(command)
-
-    command = 'cdo remapbil,r360x180 {}ta_{}_{}_2015-2100_LTmean.nc {}ta_{}_{}_2015-2100_LTmean_r1.nc'.format(cartlt, mod, mem, cart_g_lt, mod, mem)
-    os.system(command)
+        command = 'cdo remapbil,r360x180 {}ta_{}_{}_2015-2100_LTmean.nc {}ta_{}_{}_{}_2015-2100_LTmean_r1.nc'.format(cartlt, mod, mem, cart_g_lt, mod, mem, ssp)
+        os.system(command)
--- pretreat_cdo_wilma_ua.py
+++ pretreat_cdo_wilma_ua.py
@@ -21,52 +21,57 @@ import pandas as pd
 
 #################################
 
-# cart_orig = '/archive/paolo/cmip5/CMIP5/output1/'
-# listacarts = glob.glob(cart_orig + '*/*/rcp85/mon/atmos/Amon/r1i1*/ua/')
-cart_orig = '/archive/paolo/cmip6/CMIP6/model-output/'
-listacarts = glob.glob(cart_orig + '*/*/ssp585/atmos/Amon/r1i1*/ua/')
-ssp = 'ssp585'
-
-cartou = '/home/federico/work/CMIP6/data_mon_ua/'
-cart_g_s = cartou + 'Smean/'
-ctl.mkdir(cart_g_s)
-
-for cart in listacarts:
-    mod = cart.split('/')[7]
-    # mem = cart.split('/')[12]
-    mem = cart.split('/')[11]
-    print('----------------------------\n')
-    print(mod, mem)
-    print('----------------------------\n')
-
-    cartstrat = cart_g_s + '{}_{}_ssp585/'.format(mod, mem)
-    ctl.mkdir(cartstrat)
-
-    file_list = [co.split('/')[-1] for co in glob.glob(cart+'ua*nc')]
-
-    for filenam in file_list:
-        if float(filenam.split('_')[-1][:-3].split('-')[1][:4]) <= 2100:
-            pass
-        else:
-            continue
-
-        file_in = cart + filenam
-        indpo = filenam.index('.nc')
-
-        filepart = filenam[:indpo] + '_Smean.nc'
-        file_out = cartstrat + filepart
-        command = 'cdo sellevel,25000,20000,15000,10000,7000,5000,3000 {} prov.nc'.format(file_in)
-        os.system(command)
-        command = 'cdo vertmean prov.nc {}'.format(file_out)
-        os.system(command)
-
-    if len(file_list) > 1:
-        command = 'cdo cat {}*Smean.nc {}ua_{}_{}_2015-2100_Smean.nc'.format(cartstrat, cartstrat, mod, mem)
-        os.system(command)
+for ssp in ['ssp585', 'rcp85']:
+    if ssp == 'rcp85':
+        cart_orig = '/archive/paolo/cmip5/CMIP5/output1/'
+        listacarts = glob.glob(cart_orig + '*/*/rcp85/mon/atmos/Amon/r1i1*/ua/')
     else:
-        command = 'cp {}*Smean.nc {}ua_{}_{}_2015-2100_Smean.nc'.format(cartstrat, cartstrat, mod, mem)
-        os.system(command)
+        cart_orig = '/archive/paolo/cmip6/CMIP6/model-output/'
+        listacarts = glob.glob(cart_orig + '*/*/ssp585/atmos/Amon/r1i1*/ua/')
 
-    # regrid
-    command = 'cdo remapbil,r360x180 {}ua_{}_{}_2015-2100_Smean.nc {}ua_{}_{}_2015-2100_Smean_r1.nc'.format(cartstrat, mod, mem, cart_g_s, mod, mem)
-    os.system(command)
+    cartou = '/home/federico/work/CMIP6/data_mon_ua/'
+    cart_g_s = cartou + 'Smean/'
+    ctl.mkdir(cart_g_s)
+
+    for cart in listacarts:
+        mod = cart.split('/')[7]
+        if ssp == 'rcp85':
+            mem = cart.split('/')[12]
+        else:
+            mem = cart.split('/')[11]
+
+        print('----------------------------\n')
+        print(mod, mem)
+        print('----------------------------\n')
+
+        cartstrat = cart_g_s + '{}_{}_{}/'.format(mod, mem, ssp)
+        ctl.mkdir(cartstrat)
+
+        file_list = [co.split('/')[-1] for co in glob.glob(cart+'ua*nc')]
+
+        for filenam in file_list:
+            if float(filenam.split('_')[-1][:-3].split('-')[1][:4]) <= 2100:
+                pass
+            else:
+                continue
+
+            file_in = cart + filenam
+            indpo = filenam.index('.nc')
+
+            filepart = filenam[:indpo] + '_Smean.nc'
+            file_out = cartstrat + filepart
+            command = 'cdo sellevel,25000,20000,15000,10000,7000,5000,3000 {} {}prov.nc'.format(file_in, cartstrat)
+            os.system(command)
+            command = 'cdo vertmean {}prov.nc {}'.format(cartstrat, file_out)
+            os.system(command)
+
+        if len(file_list) > 1:
+            command = 'cdo cat {}*Smean.nc {}ua_{}_{}_2015-2100_Smean.nc'.format(cartstrat, cartstrat, mod, mem)
+            os.system(command)
+        else:
+            command = 'cp {}*Smean.nc {}ua_{}_{}_2015-2100_Smean.nc'.format(cartstrat, cartstrat, mod, mem)
+            os.system(command)
+
+        # regrid
+        command = 'cdo remapbil,r360x180 {}ua_{}_{}_2015-2100_Smean.nc {}ua_{}_{}_{}_2015-2100_Smean_r1.nc'.format(cartstrat, mod, mem, cart_g_s, mod, mem, ssp)
+        os.system(command)
