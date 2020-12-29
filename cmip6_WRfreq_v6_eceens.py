8766281f8bf11a478429e4d5604afa56be737d3f
Federico Fabiano <f.fabiano@isac.cnr.it>
2020-12-29 12:08:27 +0100

    ece ens

--- cmip6_WRfreq_v6_eceens.py
+++ cmip6_WRfreq_v6_eceens.py
@@ -33,14 +33,15 @@ cart_cmip5 = '/home/fabiano/Research/lavori/CMIP6/Results_cmip5/{}_NDJFM/'
 yr10 = 10 # length of running mean
 #dtrtyp = 'light'
 dtrtyp = 'histrebase'
-cart_out_orig = '/home/fabiano/Research/lavori/CMIP6/Results_v6_eceens/'
+cart_out_orig = '/home/fabiano/Research/lavori/CMIP6/Results_v6_eceens/no_rebase/'
 ctl.mkdir(cart_out_orig)
 
 cart_in = '/data-hobbes/fabiano/WR_CMIP6/'
 #file_hist = cart_in + 'out_NEW_cmip6_hist_NDJFM_{}_4clus_4pcs_1964-2014_refCLUS_dtr_light.p'
 file_hist = cart_in + 'out_eceens_hist_NDJFM_{}_4clus_4pcs_1964-2014_refCLUS_dtr.p'
 file_hist_refEOF = cart_in + 'out_eceens_hist_NDJFM_{}_4clus_4pcs_1964-2014_refEOF_dtr.p'
-gen_file_ssp = cart_in + 'out_eceens_{}_NDJFM_{}_4clus_4pcs_2015-2100_refCLUS_dtr_reb.p'
+#gen_file_ssp = cart_in + 'out_eceens_{}_NDJFM_{}_4clus_4pcs_2015-2100_refCLUS_dtr_reb.p'
+gen_file_ssp = cart_in + 'out_eceens_{}_NDJFM_{}_4clus_4pcs_2015-2100_refCLUS_dtr.p'
 
 numclus = 4
 reg_names_area = dict()
