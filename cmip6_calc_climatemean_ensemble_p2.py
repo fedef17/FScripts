d1615af513eabb44b9c12dbda118e4b66a46052e
Federico Fabiano <f.fabiano@isac.cnr.it>
2021-01-05 14:52:49 +0100

    check climate_mean historical

--- cmip6_calc_climatemean_ensemble_p2.py
+++ cmip6_calc_climatemean_ensemble_p2.py
@@ -205,8 +205,9 @@ for area in ['EAT', 'PNA']:
     res_old, _ = ctl.load_wrtool('/data-hobbes/fabiano/WR_CMIP6/out_NEW_cmip6_hist_NDJFM_{}_4clus_4pcs_1964-2014_refCLUS_dtr_light.p'.format(area))
     figs = []
     for mod in okmods_mo:
+        modmem = [ke for ke in res_old.keys() if mod in ke][0]
         newcoso = np.mean(climate_mean[(area, mod)], axis = 0)
-        oldcoso = np.mean(res_old[mod]['climate_mean'], axis = 0)
+        oldcoso = np.mean(res_old[modmem]['climate_mean'], axis = 0)
         lat = res_old[mod]['lat']
         lon = res_old[mod]['lon']
         fig = ctl.plot_map_contour(newcoso-oldcoso, lat, lon, filename = None, visualization = 'standard', central_lat_lon = None, cmap = 'RdBu_r', title = mod, xlabel = None, ylabel = None, cb_label = None, cbar_range = (-60, 60), plot_anomalies = True, n_color_levels = 21, draw_contour_lines = False, n_lines = 5, color_percentiles = (0,100), bounding_lat = 30, plot_margins = area, add_rectangles = None, draw_grid = True, plot_type = 'filled_contour', verbose = False, lw_contour = 0.5)
