79f9d50ac5766e87bd97b49f900e4a640cc515c3
Federico Fabiano <f.fabiano@isac.cnr.it>
2021-04-13 11:22:38 +0200

    added bottino_overview

--- bottino_overview.py
+++ bottino_overview.py
@@ -30,20 +30,60 @@ plt.rcParams['axes.labelsize'] = 18
 plt.rcParams['axes.axisbelow'] = True
 
 #############################################################################
-
-# filna = '/nas/BOTTINO/b{}/cmorized/cmor_*/CMIP6/LongRunMIP/EC-Earth-Consortium/EC-Earth3/stabilization-ssp585-2{}/r1i1p1f1/{}/{}/gr/v20210315/*nc'
-#
-# filist = glob.glob(filna.format('025', '025', 'Amon', 'pr'))
-#
-# gigi = xr.open_mfdataset(filist)
-
 import glob
 import xarray as xr
 import xclim
 
-filist = glob.glob('/nas/BOTTINO/b100/cmorized/cmor_2*/CMIP6/LongRunMIP/EC-Earth-Consortium/EC-Earth3/stabilization-ssp585-2100/r1i1p1f1/SImon/siconc/gn/v20210315/*nc')
-gigi = xr.open_mfdataset(filist, use_cftime=True)
+filna = '/nas/BOTTINO/{}/cmorized/cmor_*/CMIP6/LongRunMIP/EC-Earth-Consortium/EC-Earth3/stabilization-ssp585-2{}/r1i1p1f1/{}/{}/gr/v20210315/*nc'
 
-areacelfi = '/nas/BOTTINO/b025/cmorized/cmor_2025/CMIP6/LongRunMIP/EC-Earth-Consortium/EC-Earth3/stabilization-ssp585-2025/r1i1p1f1/fx/areacello/gn/v20210315/areacella_fx_EC-Earth3_stabilization-ssp585-2025_r1i1p1f1_gn.nc'
+# filist = glob.glob('/nas/BOTTINO/b100/cmorized/cmor_2*/CMIP6/LongRunMIP/EC-Earth-Consortium/EC-Earth3/stabilization-ssp585-2100/r1i1p1f1/SImon/siconc/gn/v20210315/*nc')
+
+allru = ['b025', 'b050', 'b100']
+colors = ['forestgreen', 'orange', 'violet']
+
+#############################################################################
+## SEA ICE
+areacelfi = '/nas/BOTTINO/areas.nc'
 acel = xr.open_dataset(areacelfi)
-pinuc = xclim.indices.sea_ice_area(gigi, acel.data)
+areaT = np.array(acel['O1t0.srf'].data)
+okslat = lat > 40.
+
+miptab = 'SImon'
+var = 'siconc'
+
+fig = plt.figure(figsize = (24,12))
+ax1 = plt.subplot(1, 2, 1)
+ax2 = plt.subplot(1, 2, 2)
+
+for ru, col in zip(allru, colors):
+    filist = glob.glob(filna.format(ru, ru[1:], miptab, var))
+    gigi = xr.open_mfdataset(filist, use_cftime=True)
+
+    seaice = np.array(gigi.siconc.data)
+    lat = np.array(gigi.latitude.data)
+
+    oksi = seaice[:, okslat]
+    oksi[oksi < 15.] = 0.
+    oksi[oksi > 15.] = 1.
+    oksiarea = oksi*areaok[np.newaxis, :]
+    seaicearea = np.nansum(oksiarea, axis = 1)
+
+    date = np.array(gigi.time.data)
+    okmarch = np.array([da.month == 3 for da in date])
+    oksept = np.array([da.month == 9 for da in date])
+
+    ax1.plot_date(date[okmarch], seaicearea[okmarch], linestyle='solid', marker = 'None', color = col, label = ru)
+    ax2.plot_date(date[oksept], seaicearea[oksept], linestyle='solid', marker = 'None', color = col, label = ru)
+
+ax1.set_title('March')
+ax2.set_title('September')
+ax2.legend()
+fig.savefig(cart_out + 'bottseaice.pdf')
+
+#pinuc = xclim.indices.sea_ice_area(gigi, acel.data)
+
+### Mean state temperature e varianza?
+
+### Mean state precipitazione e varianza
+
+### Mean state wind e varianza
