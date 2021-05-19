0dc8b671544b47c44741df679ce0cfd42635bfc3
Federico Fabiano <f.fabiano@isac.cnr.it>
2021-05-19 15:06:24 +0200

    test new reg

--- test_new_clustering.py
+++ test_new_clustering.py
@@ -38,26 +38,82 @@ cart_out = '/home/fabiano/Research/lavori/WeatherRegimes/ERA5_tests/'
 # pickle.dump([ua, zg, coords], open(cart_out + 'zg_e_ua_ERA5.p', 'wb'))
 ua, zg, coords = pickle.load(open(cart_out + 'zg_e_ua_ERA5.p', 'rb'))
 
+zg_zon = np.mean(zg, axis = -1)
+zg_eddy = zg - zg_zon[..., np.newaxis]
 
-ua_climate_mean, ua_dates_climate_mean, _ = ctl.daily_climatology(ua, coords['dates'], window = 20)
-zg_climate_mean, zg_dates_climate_mean, _ = ctl.daily_climatology(zg, coords['dates'], window = 20)
+ua_climate_mean, dates_climate_mean, _ = ctl.daily_climatology(ua, coords['dates'], window = 20)
+zg_climate_mean, dates_climate_mean, _ = ctl.daily_climatology(zg, coords['dates'], window = 20)
+zg_climate_mean_eddy, dates_climate_mean, _ = ctl.daily_climatology(zg_eddy, coords['dates'], window = 20)
+zg_climate_mean_zon, dates_climate_mean, _ = ctl.daily_climatology(zg_zon, coords['dates'], window = 20)
 
 ua_low = ctl.butter_filter(ua, 10)
 ua_low_djfm, dates_djfm = ctl.sel_season(ua_low, coords['dates'], 'DJFM')
 
+zg_djfm, dates_djfm = ctl.sel_season(zg, coords['dates'], 'DJFM')
+zg_eddy_djfm, dates_djfm = ctl.sel_season(zg_eddy, coords['dates'], 'DJFM')
+
+# zg_low = ctl.butter_filter(zg, 10)
+# zg_low_djfm, dates_djfm = ctl.sel_season(zg_low, coords['dates'], 'DJFM')
+#
+# zg_low_eddy = ctl.butter_filter(zg_eddy, 10)
+# zg_low_eddy_djfm, dates_djfm = ctl.sel_season(zg_low_eddy, coords['dates'], 'DJFM')
+
 ### Test jli.
 figs = []
-jli, jspeed, jdates = cd.jetlatindex(ua, coords['lat'], coords['lon'], coords['dates'], filter = 'butter')
-figs.append(cd.plot_jli_w_speed(jli, jspeed, jdates, title = 'butterworth w 0.22'))
-jli, jspeed, jdates = cd.jetlatindex(ua, coords['lat'], coords['lon'], coords['dates'], filter = 'butter')
-figs.append(cd.plot_jli_w_speed(jli, jspeed, jdates, title = 'butterworth w auto bnd', bnd_width = None))
-jli, jspeed, jdates = cd.jetlatindex(ua, coords['lat'], coords['lon'], coords['dates'], filter = 'butter', remove_orog = True)
-figs.append(cd.plot_jli_w_speed(jli, jspeed, jdates, title = 'butterworth no greenland'))
-jli, jspeed, jdates = cd.jetlatindex(ua, coords['lat'], coords['lon'], coords['dates'], filter = 'lanczos')
-figs.append(cd.plot_jli_w_speed(jli, jspeed, jdates, title = 'lanczos'))
-ctl.plot_pdfpages(cart_out + 'test_jli_filters.pdf', figs)
+# jli, jspeed, jdates = cd.jetlatindex(ua, coords['lat'], coords['lon'], coords['dates'], filter = 'butter')
+# figs.append(cd.plot_jli_w_speed(jli, jspeed, jdates, title = 'butterworth w 0.22'))
+# jli, jspeed, jdates = cd.jetlatindex(ua, coords['lat'], coords['lon'], coords['dates'], filter = 'butter')
+# figs.append(cd.plot_jli_w_speed(jli, jspeed, jdates, title = 'butterworth w auto bnd', bnd_width = None))
+# jli, jspeed, jdates = cd.jetlatindex(ua, coords['lat'], coords['lon'], coords['dates'], filter = 'butter', remove_orog = True)
+# figs.append(cd.plot_jli_w_speed(jli, jspeed, jdates, title = 'butterworth no greenland'))
+# jli, jspeed, jdates = cd.jetlatindex(ua, coords['lat'], coords['lon'], coords['dates'], filter = 'lanczos')
+# figs.append(cd.plot_jli_w_speed(jli, jspeed, jdates, title = 'lanczos'))
+# ctl.plot_pdfpages(cart_out + 'test_jli_filters.pdf', figs)
+
+lat = coords['lat']
+lon = coords['lon']
+
+res_all = dict()
+
+area = 'NATL'
+numpcs = 5
+numclus = 5
+perc = None
+
+res_all['ua_low_full_5'] = cd.WRtool_core(ua_low_djfm, lat, lon, dates_season, area, numpcs = numpcs, perc = perc, numclus = numclus, remove_climate_mean = False)
+
+res_all['ua_low_anom_5'] = cd.WRtool_core(ua_low_djfm, lat, lon, dates_season, area, climate_mean = ua_climate_mean, dates_climate_mean = dates_climate_mean, numpcs = numpcs, perc = perc, numclus = numclus, remove_climate_mean = True)
+
+numclus = 4
+res_all['ua_low_full_4'] = cd.WRtool_core(ua_low_djfm, lat, lon, dates_season, area, numpcs = numpcs, perc = perc, numclus = numclus, remove_climate_mean = False)
+
+res_all['ua_low_anom_4'] = cd.WRtool_core(ua_low_djfm, lat, lon, dates_season, area, climate_mean = ua_climate_mean, dates_climate_mean = dates_climate_mean, numpcs = numpcs, perc = perc, numclus = numclus, remove_climate_mean = True)
 
 
+area = 'EAT'
+numpcs = 4
+numclus = 4
 
+res_all['zg_eddy_full'] = cd.WRtool_core(zg_eddy_djfm, lat, lon, dates_season, area, numpcs = numpcs, perc = perc, numclus = numclus, remove_climate_mean = False)
 
-#results = cd.WRtool_core(var_season, lat, lon, dates_season, area, climate_mean = climate_mean, dates_climate_mean = dates_climate_mean, numpcs = inputs['numpcs'], perc = inputs['perc'], numclus = inputs['numclus'])
+res_all['zg_eddy_anom'] =  cd.WRtool_core(zg_eddy_djfm, lat, lon, dates_season, area, climate_mean = zg_climate_mean_eddy, dates_climate_mean = dates_climate_mean, numpcs = numpcs, perc = perc, numclus = numclus, remove_climate_mean = True)
+
+res_all['zg_full'] = cd.WRtool_core(zg_djfm, lat, lon, dates_season, area, numpcs = numpcs, perc = perc, numclus = numclus, remove_climate_mean = False)
+
+res_all['zg_anom'] = cd.WRtool_core(zg_djfm, lat, lon, dates_season, area, climate_mean = zg_climate_mean, dates_climate_mean = dates_climate_mean, numpcs = numpcs, perc = perc, numclus = numclus, remove_climate_mean = True)
+
+res_all['zg_zon_anom'] = cd.WRtool_core(zg_djfm, lat, lon, dates_season, area, climate_mean = zg_climate_mean_zon[..., np.newaxis], dates_climate_mean = dates_climate_mean, numpcs = numpcs, perc = perc, numclus = numclus, remove_climate_mean = True)
+
+figs = []
+for ke in res_all:
+    koze = res_all[ke]
+    if 'ua' in ke:
+        cbar_range = (-30, 30.)
+        cb_label = 'u (m/s)'
+    else:
+        cbar_range = (-135, 135.)
+        cb_label = 'zg (m)'
+    fig = cd.plot_regimes(koze['lat'], koze['lon'], koze['cluspattern'], None, cbar_range = cbar_range, cb_label = cb_label)
+    figs.append(fig)
+
+ctl.plot_pdfpages(cart_out + 'test_regimes_pattern.pdf', figs)
