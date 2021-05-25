dc8155e0d10d66eed5e1e407589c65846f944378
Federico Fabiano <f.fabiano@isac.cnr.it>
2021-05-25 11:41:10 +0200

    bottino 2

--- bottino_overview_2.py
+++ bottino_overview_2.py
@@ -31,7 +31,8 @@ plt.rcParams['axes.axisbelow'] = True
 
 #############################################################################
 
-cart_out = '/home/fabiano/Research/lavori/BOTTINO/analisi/'
+cart_out = '/home/fabiano/Research/lavori/BOTTINO/seasmean/'
+ctl.mkdir(cart_out)
 
 #filna = '/nas/BOTTINO/{}/cmorized/cmor_*/CMIP6/LongRunMIP/EC-Earth-Consortium/EC-Earth3/stabilization-ssp585-2{}/r1i1p1f1/{}/{}/*/v20210315/*nc'
 
@@ -56,58 +57,58 @@ varnam = 'siconc'
 
 resdict = dict()
 
-fig = plt.figure(figsize = (24,12))
-ax1 = plt.subplot(1, 2, 1)
-ax2 = plt.subplot(1, 2, 2)
-
-for na, ru, col in zip(allnams + ['ssp585'], allru + ['ssp585'], colors + ['indianred']):
-    mem = 'r1'
-    if na == 'ssp585': mem = 'r4'
-    filist = glob.glob(filna.format(na, mem, miptab, varnam))
-    gigi = xr.open_mfdataset(filist, use_cftime=True)
-
-    seaice = np.array(gigi.siconc.data)
-    lat = np.array(gigi.latitude.data)
-    okslat = lat > 40.
-
-    areaok = areaT[okslat]
-    oksi = seaice[:, okslat]
-    oksi[oksi < 15.] = 0.
-    oksi[oksi > 15.] = 1.
-    oksiarea = oksi*areaok[np.newaxis, :]
-    seaicearea = np.nansum(oksiarea, axis = 1)
-
-    dates = np.array(gigi.time.data)
-    okmarch = np.array([da.month == 3 for da in dates])
-    oksept = np.array([da.month == 9 for da in dates])
-
-    resdict[(ru, varnam, 'glomean', 'mar')] = seaicearea[okmarch]
-    resdict[(ru, varnam, 'glomean', 'sep')] = seaicearea[oksept]
-
-    ax1.plot_date(dates[okmarch], seaicearea[okmarch], linestyle='solid', marker = 'None', color = col, label = ru)
-    ax2.plot_date(dates[oksept], seaicearea[oksept], linestyle='solid', marker = 'None', color = col, label = ru)
-
-    # if ru == 'ssp585':
-    #     continue
-    #
-    # if ru != 'pi':
-    #     ok200 = np.array([da.year > dates[-1].year-200 for da in dates])
-    #     varok = var[ok200]
-    #     dateok = dates[ok200]
-    # else:
-    #     varok = var
-    #     dateok = dates
-    #
-    # resdict[(ru, varnam, 'mean', 'mar')], resdict[(ru, varnam, 'std', 'mar')] = ctl.seasonal_climatology(varok, dateok, 'Mar')
-    # resdict[(ru, varnam, 'mean', 'sep')], resdict[(ru, varnam, 'std', 'sep')] = ctl.seasonal_climatology(varok, dateok, 'Sep')
-
-
-ax1.set_title('March')
-ax2.set_title('September')
-ax1.set_ylabel(r'Sea ice extent (m$^2$)')
-ax2.set_ylabel(r'Sea ice extent (m$^2$)')
-ax2.legend()
-fig.savefig(cart_out + 'bottseaice.pdf')
+# fig = plt.figure(figsize = (24,12))
+# ax1 = plt.subplot(1, 2, 1)
+# ax2 = plt.subplot(1, 2, 2)
+#
+# for na, ru, col in zip(allnams + ['ssp585'], allru + ['ssp585'], colors + ['indianred']):
+#     mem = 'r1'
+#     if na == 'ssp585': mem = 'r4'
+#     filist = glob.glob(filna.format(na, mem, miptab, varnam))
+#     gigi = xr.open_mfdataset(filist, use_cftime=True)
+#
+#     seaice = np.array(gigi.siconc.data)
+#     lat = np.array(gigi.latitude.data)
+#     okslat = lat > 40.
+#
+#     areaok = areaT[okslat]
+#     oksi = seaice[:, okslat]
+#     oksi[oksi < 15.] = 0.
+#     oksi[oksi > 15.] = 1.
+#     oksiarea = oksi*areaok[np.newaxis, :]
+#     seaicearea = np.nansum(oksiarea, axis = 1)
+#
+#     dates = np.array(gigi.time.data)
+#     okmarch = np.array([da.month == 3 for da in dates])
+#     oksept = np.array([da.month == 9 for da in dates])
+#
+#     resdict[(ru, varnam, 'glomean', 'mar')] = seaicearea[okmarch]
+#     resdict[(ru, varnam, 'glomean', 'sep')] = seaicearea[oksept]
+#
+#     ax1.plot_date(dates[okmarch], seaicearea[okmarch], linestyle='solid', marker = 'None', color = col, label = ru)
+#     ax2.plot_date(dates[oksept], seaicearea[oksept], linestyle='solid', marker = 'None', color = col, label = ru)
+#
+#     # if ru == 'ssp585':
+#     #     continue
+#     #
+#     # if ru != 'pi':
+#     #     ok200 = np.array([da.year > dates[-1].year-200 for da in dates])
+#     #     varok = var[ok200]
+#     #     dateok = dates[ok200]
+#     # else:
+#     #     varok = var
+#     #     dateok = dates
+#     #
+#     # resdict[(ru, varnam, 'mean', 'mar')], resdict[(ru, varnam, 'std', 'mar')] = ctl.seasonal_climatology(varok, dateok, 'Mar')
+#     # resdict[(ru, varnam, 'mean', 'sep')], resdict[(ru, varnam, 'std', 'sep')] = ctl.seasonal_climatology(varok, dateok, 'Sep')
+#
+#
+# ax1.set_title('March')
+# ax2.set_title('September')
+# ax1.set_ylabel(r'Sea ice extent (m$^2$)')
+# ax2.set_ylabel(r'Sea ice extent (m$^2$)')
+# ax2.legend()
+# fig.savefig(cart_out + 'bottseaice.pdf')
 
 # coso = gigi['siconc'].sel(time = gigi['time.month'] == 3).mean('time')
 # fig, ax = ctl.get_cartopy_fig_ax('nearside', (90., 0.), 30.)
@@ -126,47 +127,124 @@ fig.savefig(cart_out + 'bottseaice.pdf')
 
 miptab = 'Amon'
 allvars_2D = 'clt  pr  psl  rlut  rsdt  rsut  tas  uas'.split()
+allvars_3D = 'ta ua'.split()
 
-exp = 'b025'
-'stabilization-ssp585-2025'
-for exp in
-if exp == 'pi'
-fils = np.concatenate([glob.glob(filna.format(exp, mem, miptab, var)) for var in allvars_2D])
-
-kose = xr.open_mfdataset(fils, use_cftime = True)
-kose = kose.drop_vars('time_bnds')
-
-sys.exit()
-
-var_glob_mean = 'tas pr clt net_toa'.split()  # plot global timeseries, including ssp585
+var_glob_mean = 'tas pr clt rlut rsut net_toa'.split()  # plot global timeseries, including ssp585
 var_map_200 = 'clt pr tas rlut uas'.split()  # plot last 200 mean map, stddev, low/high var wrt pi
 
+allnams2 = allnams + ['ssp585']
+allru2 = allru + ['ssp585']
+colors2 = colors + ['indianred']
+
+figs_glob = []
+axs_glob = []
+pimean = dict()
+glomeans = dict()
+mapmean = dict()
 
 for var in var_glob_mean:
-    ### Add global mean timeseries
-    fig = plt.figure()
+    fig, ax = plt.subplots(figsize = (12,8))
+    axs_glob.append(ax)
+    figs_glob.append(fig)
+    ax.set_title(var)
 
-    for na, ru, col in zip(allnams2, allru2, colors2):
-        mem = 'r1'
-        if na == 'ssp585': mem = 'r4'
+fig_greg, ax_greg = plt.subplots(figsize = (12,8))
 
-        fils = glob.glob(filna.format(na, mem, miptab, var))
+#for na, ru, col in zip(allnams2, allru2, colors2):
+for na, ru, col in zip(allnams, allru, colors):
+    mem = 'r1'
+    if na == 'ssp585': mem = 'r4'
 
-        kose = xr.open_mfdataset(fils, use_cftime = True)
-        kose = kose.drop_vars('time_bnds')
+    fils = np.concatenate([glob.glob(filna.format(exp, mem, miptab, var)) for var in allvars_2D])
+
+    kose = xr.open_mfdataset(fils, use_cftime = True)
+    kose = kose.drop_vars('time_bnds')
+
+    kose = kose.assign(net_toa = kose.rsdt - kose.rlut - kose.rsut) # net downward energy flux at TOA
+
+    for var, fig, ax in zip(var_glob_mean, figs_glob, axs_glob):
+        if var not in kose:
+            if ru == 'pi':
+                pimean[var] = 0.
+            continue
 
         coso = kose[var].mean('lon').groupby("time.year").mean()
         glomean = np.average(coso, weights = abs(np.cos(np.deg2rad(coso.lat))), axis = -1)
         if ru == 'pi':
             years = coso.year.data-2256+2015
+            pimean[var] = np.mean(glomean)
         else:
             years = coso.year.data
-        plt.plot(coso.year.data, glomean, label = ru, color = col)
 
-    plt.grid()
-    plt.title(var)
-    plt.legend()
-    figs_global.append(fig)
+        glomeans[(ru, var)] = (years, glomean)
+        ax.plot(years, glomean-pimean[var], label = ru, color = col)
+
+        if ru == 'b100':
+            ax.legend()
+            ax.grid()
+
+    # gregory
+    ax_greg.plot(glomeans[(ru, 'tas')][1]-pimean['tas'], glomeans[(ru, 'net_toa')][1], label = ru, color = col)
+    if ru == 'b100':
+        ax_greg.legend()
+        ax_greg.grid()
+
+    if ru == 'ssp585':
+        continue
+
+    if ru != 'pi':
+        kose = kose.sel(time = kose['time.year'] >= kose.year.data[-1]-200)
+
+    kose_smean = kose.groupby("time.season").mean()
+    kose_sstd = kose.groupby("time.season").std()
+    kose_p90 = kose.groupby("time.season").percentile(90)
+    kose_p10 = kose.groupby("time.season").percentile(10)
+
+    for var in var_map_200:
+        mapmean[(ru, var, 'mean')] = kose_smean
+        mapmean[(ru, var, 'std')] = kose_sstd
+        mapmean[(ru, var, 'p90')] = kose_p90
+        mapmean[(ru, var, 'p10')] = kose_p10
+
+    # for var in var_map_200:
+    #     print(var)
+    #     vmax = np.nanpercentile(flux_season[var], 98)
+    #     fig = plt.figure()
+    #     guplo = flux_season[var].plot.contourf(col = 'season', col_wrap = 2, levels = 11, vmax = vmax, transform = proj, figsize = (16,12), subplot_kws = {"projection": proj})
+    #     guplo.map(lambda: plt.gca().coastlines())
+    #     plt.title(var)
+    #     plt.savefig(cart + '{}_seas_{}.pdf'.format(mod, var))
+
+
+ctl.plot_pdfpages(cart_out + 'bottino_glomeans.pdf', figs_glob)
+fig_greg.savefig(cart_out + 'bottino_gregory.pdf')
+
+
+# 3D vars
+for na, ru, col in zip(allnams, allru, colors):
+    mem = 'r1'
+    if na == 'ssp585': mem = 'r4'
+
+    fils = np.concatenate([glob.glob(filna.format(exp, mem, miptab, var)) for var in allvars_3D])
+
+    kose = xr.open_mfdataset(fils, use_cftime = True)
+    kose = kose.drop_vars('time_bnds')
+
+    if ru != 'pi':
+        kose = kose.sel(time = kose['time.year'] >= kose.year.data[-1]-200)
+
+    kose_smean = kose.groupby("time.season").mean()
+    kose_sstd = kose.groupby("time.season").std()
+    kose_p90 = kose.groupby("time.season").percentile(90)
+    kose_p10 = kose.groupby("time.season").percentile(10)
+
+    for var in var_map_200:
+        mapmean[(ru, var, 'mean')] = kose_smean
+        mapmean[(ru, var, 'std')] = kose_sstd
+        mapmean[(ru, var, 'p90')] = kose_p90
+        mapmean[(ru, var, 'p10')] = kose_p10
+
+pickle.dump([glomeans, pimean, mapmean], open(cart_out + 'bottino_seasmean.pdf', 'wb'))
 
 sys.exit()
 
