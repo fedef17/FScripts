1ab44108b61d9a8fd7965a1d6a4aa76cc56fcaf6
Federico Fabiano <f.fabiano@isac.cnr.it>
2021-05-28 17:52:39 +0200

    bottino jli

--- bottino_jetlat.py
+++ bottino_jetlat.py
@@ -53,8 +53,9 @@ areasel['SPAC'] = [180., 240., -70., -20.]
 areasel['SATL'] = [-45., 10., -70., -20.]
 areasel['SIND'] = [50., 110., -70., -20.]
 
-resdict = dict()
+allseas = ['DJF', 'MAM', 'JJA', 'SON']
 
+resdict = dict()
 for na, ru, col in zip(allnams2, allru2, colors2):
     print(ru)
     mem = 'r1i1p1f1'
@@ -70,12 +71,13 @@ for na, ru, col in zip(allnams2, allru2, colors2):
     #     resdict[(ru, 'jli', area)] = jli
     #     resdict[(ru, 'jspeed', area)] = jspeed
     #     resdict[(ru, 'dates')] = dates
-    jli, jspeed, dates = cd.jli_from_files(filist, allareadict = areasel)
+    for season in allseas:
+        jli, jspeed, dates = cd.jli_from_files(filist, allareadict = areasel, season = season)
 
-    for area in areasel.keys():
-        resdict[(ru, 'jli', area)] = jli[area]
-        resdict[(ru, 'jspeed', area)] = jspeed[area]
-    resdict[(ru, 'dates')] = dates
+        for area in areasel.keys():
+            resdict[(ru, 'jli', area, season)] = jli[area]
+            resdict[(ru, 'jspeed', area, season)] = jspeed[area]
+        resdict[(ru, 'dates', season)] = dates
 
 with open(cart_out + 'res_jli200_v2.p', 'wb') as filox:
     pickle.dump(resdict, filox)
@@ -89,56 +91,60 @@ for tip, aruok, colok in zip(['', '_wc1950'], [allru, allru2], [colors, colors2]
     iS = 0
     iN = 0
     for area in areasel.keys():
-        fig = plt.figure(figsize = (24,12))
-        ax1 = fig.add_subplot(121)
-        ax2 = fig.add_subplot(122)
+        for season in allseas:
+            fig = plt.figure(figsize = (24,12))
+            ax1 = fig.add_subplot(121)
+            ax2 = fig.add_subplot(122)
 
-        if area[0] == 'N':
-            axlu = axN[iN]
-            iN += 1
-        else:
-            axlu = axS[iS]
-            iS += 1
+            if area[0] == 'N' and season == 'DJF':
+                axlu = axN[iN]
+                iN += 1
+            elif area[0] == 'S' and season == 'JJA':
+                axlu = axS[iS]
+                iS += 1
+            else:
+                axlu = None
 
-        latsel = np.arange(areasel[area][-2], areasel[area][-1]+0.1, 0.5)
-        kos = np.concatenate([resdict[(ru, 'jspeed', area)] for ru in aruok])
-        vmin, vmax = (np.min(kos), np.max(kos))
-        print(vmin, vmax)
-        vsel = np.linspace(vmin, vmax, 100)
+            latsel = np.arange(areasel[area][-2], areasel[area][-1]+0.1, 0.5)
+            kos = np.concatenate([resdict[(ru, 'jspeed', area, season)] for ru in aruok])
+            vmin, vmax = (np.min(kos), np.max(kos))
+            print(vmin, vmax)
+            vsel = np.linspace(vmin, vmax, 100)
 
-        def dopdf(var, xi, bnd_width):
-            pdf = ctl.calc_pdf(var, bnd_width = bnd_width)
-            pdfok = pdf(xi)
-            pdfok /= np.sum(pdfok)
-            return pdfok
+            def dopdf(var, xi, bnd_width):
+                pdf = ctl.calc_pdf(var, bnd_width = bnd_width)
+                pdfok = pdf(xi)
+                pdfok /= np.sum(pdfok)
+                return pdfok
 
-        for ru, col in zip(aruok, colok):
-            jli = resdict[(ru, 'jli', area)]
-            jspeed = resdict[(ru, 'jspeed', area)]
+            for ru, col in zip(aruok, colok):
+                jli = resdict[(ru, 'jli', area, season)]
+                jspeed = resdict[(ru, 'jspeed', area, season)]
 
-            jliserie = ctl.bootstrap(jli, resdict[(ru, 'dates')], None, apply_func = dopdf, func_args = [latsel, 0.22], n_choice = 50, n_bootstrap = 1000)
-            jspedserie = ctl.bootstrap(jspeed, resdict[(ru, 'dates')], None, apply_func = dopdf, func_args = [vsel, None], n_choice = 50, n_bootstrap = 1000)
+                jliserie = ctl.bootstrap(jli, resdict[(ru, 'dates', season)], None, apply_func = dopdf, func_args = [latsel, 0.22], n_choice = 50, n_bootstrap = 1000)
+                jspedserie = ctl.bootstrap(jspeed, resdict[(ru, 'dates', season)], None, apply_func = dopdf, func_args = [vsel, None], n_choice = 50, n_bootstrap = 1000)
 
-            pdf = ctl.calc_pdf(jli, bnd_width = 0.22)
-            pdfok = pdf(latsel)
-            pdfok /= np.sum(pdfok)
+                pdf = ctl.calc_pdf(jli, bnd_width = 0.22)
+                pdfok = pdf(latsel)
+                pdfok /= np.sum(pdfok)
 
-            jlimin = np.percentile(jliserie, 10, axis = 0)
-            jlimax = np.percentile(jliserie, 90, axis = 0)
-            ax1.fill_between(latsel, jlimin, jlimax, color = col, alpha = 0.3)
-            ax1.plot(latsel, pdfok, label = ru, color = col, linewidth = 3)
+                jlimin = np.percentile(jliserie, 10, axis = 0)
+                jlimax = np.percentile(jliserie, 90, axis = 0)
+                ax1.fill_between(latsel, jlimin, jlimax, color = col, alpha = 0.3)
+                ax1.plot(latsel, pdfok, label = ru, color = col, linewidth = 3)
 
-            axlu.fill_between(latsel, jlimin, jlimax, color = col, alpha = 0.3)
-            axlu.plot(latsel, pdfok, label = ru, color = col, linewidth = 3)
+                if axlu is not None:
+                    axlu.fill_between(latsel, jlimin, jlimax, color = col, alpha = 0.3)
+                    axlu.plot(latsel, pdfok, label = ru, color = col, linewidth = 3)
 
-            pdf = ctl.calc_pdf(jspeed)
-            pdfok = pdf(vsel)
-            pdfok /= np.sum(pdfok)
+                pdf = ctl.calc_pdf(jspeed)
+                pdfok = pdf(vsel)
+                pdfok /= np.sum(pdfok)
 
-            jlimin = np.percentile(jspedserie, 10, axis = 0)
-            jlimax = np.percentile(jspedserie, 90, axis = 0)
-            ax2.fill_between(vsel, jlimin, jlimax, color = col, alpha = 0.3)
-            ax2.plot(vsel, pdfok, label = ru, color = col, linewidth = 3)
+                jlimin = np.percentile(jspedserie, 10, axis = 0)
+                jlimax = np.percentile(jspedserie, 90, axis = 0)
+                ax2.fill_between(vsel, jlimin, jlimax, color = col, alpha = 0.2)
+                ax2.plot(vsel, pdfok, label = ru, color = col, linewidth = 3)
 
         axlu.grid()
         axlu.set_xlabel('Latitude')
@@ -153,7 +159,11 @@ for tip, aruok, colok in zip(['', '_wc1950'], [allru, allru2], [colors, colors2]
         ax2.set_title('Jet speed')
         ax1.legend()
         ax2.legend()
-        fig.savefig(cart_out + 'jlinspeed_bottino_{}{}.pdf'.format(area, tip))
+        fig.suptitle('{} - {}'.format(area, season))
+        #fig.savefig(cart_out + 'jlinspeed_bottino_{}{}.pdf'.format(area, tip))
+        figs.append(fig)
+
+    ctl.plot_pdfpages(cart_out + 'jlinspeed_bottinoall{}.pdf'.format(tip), figs)
 
     figN.savefig(cart_out + 'jlibott_allareas_N{}.pdf'.format(tip))
     figS.savefig(cart_out + 'jlibott_allareas_S{}.pdf'.format(tip))
