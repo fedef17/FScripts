59c91c7cd06ad198e3e033bf880b2ac1ca344810
Federico Fabiano <f.fabiano@isac.cnr.it>
2021-07-06 19:24:20 +0200

    bott regdist

--- bottino_regional_dist.py
+++ bottino_regional_dist.py
@@ -12,7 +12,7 @@ import netCDF4 as nc
 
 import climtools_lib as ctl
 import climdiags as cd
-from tunlib import gregplot_on_ax
+#from tunlib import gregplot_on_ax
 
 from matplotlib.colors import LogNorm
 from datetime import datetime
