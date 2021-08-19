0857dbfe01492bb306db0bd08a21f00cc1aec26f
Federico Fabiano <f.fabiano@isac.cnr.it>
2021-08-19 18:44:09 +0200

    kj reg w cmip5

--- kj_multireg_hist_3n.py
+++ kj_multireg_hist_3n.py
@@ -18,6 +18,7 @@ from matplotlib.colors import LogNorm
 from datetime import datetime
 
 from scipy import stats
+from copy import deepcopy as dcp
 
 from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.preprocessing import StandardScaler
@@ -238,7 +239,7 @@ def make_XY(reg, comb, models = models_all, models_ok = models_ok_all, alldrivs 
 
 for models_ok, models, ensmod in zip([models_cmip6, models_prim, models_5_ok, models_ok_cm6prim, models_ok_all], [models_cm6_full, models_prim, models_5, models_cm6prim, models_all], ['solocmip6', 'soloprim', 'solocmip5', 'cmip6prim', 'all']):
 
-    drilis = drilis_all
+    drilis = dcp(drilis_all)
     if ensmod not in ['solocmip5', 'all']:
         drilis.append(('resolution', 'atm_lev'))
         drilis.append(('resolution', 'oce_res'))
