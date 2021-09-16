0de3ee566aa8353bd23d6b0df7a6974631504933
Federico Fabiano <f.fabiano@isac.cnr.it>
2021-09-16 17:53:23 +0200

    kj exclude high correlation drivers

--- kj_multireg_hist_3n_allmem.py
+++ kj_multireg_hist_3n_allmem.py
@@ -321,7 +321,7 @@ for models_ok, models, ensmod in zip([models_ok_all], [models_all], ['all']):
                 for iko in range(len(crosscor)): crosscor[iko,iko] = 0.
 
                 if np.any(np.abs(crosscor) > 0.35):
-                    print('Discarding.. too high correlation between drivers')
+                    #print('Discarding.. too high correlation between drivers')
                     okcombs[reg].append(comb)
                     allscores[reg].append(np.zeros(len(Y.T)))
                     continue
