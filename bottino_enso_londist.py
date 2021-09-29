f338117d399374c61f630c944113a5336cfc5e0c
Federico Fabiano <f.fabiano@isac.cnr.it>
2021-09-29 20:35:37 +0200

    bott enso londi

---
+++ bottino_enso_londist.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import sys
+import os
+from matplotlib import pyplot as plt
+from matplotlib import cm
+
+import pickle
+import netCDF4 as nc
+
+import climtools_lib as ctl
+import climdiags as cd
+#from tunlib import gregplot_on_ax
+
+from matplotlib.colors import LogNorm
+from datetime import datetime
+
+from scipy import stats
+import xarray as xr
+import glob
+import xclim
+
+plt.rcParams['xtick.labelsize'] = 15
+plt.rcParams['ytick.labelsize'] = 15
+titlefont = 22
+plt.rcParams['figure.titlesize'] = titlefont
+plt.rcParams['axes.titlesize'] = 18
+plt.rcParams['axes.labelsize'] = 15
+plt.rcParams['axes.axisbelow'] = True
+
+#############################################################################
+
+if os.uname()[1] == 'hobbes':
+    cart = '/home/fabiano/Research/lavori/BOTTINO/'
+elif os.uname()[1] == 'xaru':
+    cart = '/home/fedef/Research/lavori/BOTTINO/'
+elif os.uname()[1] == 'tintin':
+    cart = '/home/fabiano/work/lavori/BOTTINO/'
+
+cart_in = cart + 'seasmean/'
+cart_out = cart + 'indices/enso/'
+ctl.mkdir(cart_out)
+
+allru = ['pi', 'b025', 'b050', 'b100']
+allnams = ['piControl', 'stabilization-ssp585-2025', 'stabilization-ssp585-2050', 'stabilization-ssp585-2100']
+
+colors = ['black', 'forestgreen', 'orange', 'violet']
+
+allnams2 = allnams + ['ssp585', 'historical']
+allru2 = allru + ['ssp585', 'hist']
+colors2 = colors + ['indianred', 'steelblue']
+####################################################################################################
+
+glomeans, pimean, yeamean, mapmean = pickle.load(open(cart_in + 'bottino_seasmean_2D.p', 'rb'))
+
+cartind = '/nas/BOTTINO/indices/enso500_xr/'
+enso = dict()
+
+for ru in allru:
+    if ru == 'pi':
+        enso[ru] = xr.load_dataset(cartind + '{}_enso_360day.nc'.format(ru), use_cftime = True)
+    else:
+        enso[ru] = xr.load_dataset(cartind + '{}_enso_360day.nc'.format(ru), use_cftime = True)
+
+        firstye = enso[ru].time.values[0].year
+        lastye = enso[ru].time.values[-1].year
+        enso[ru+'_st'] = enso[ru].sel(time = slice('{}-01-01'.format(lastye-200), '{}-12-30'.format(lastye)))
+        enso[ru+'_tr'] = enso[ru].sel(time = slice('{}-01-01'.format(firstye), '{}-12-30'.format(firstye+50)))
+
+#########################################
+
+fig_o, axs_o = plt.subplots(2, 2, figsize = (16,12))
+fig_a, axs_a = plt.subplots(2, 2, figsize = (16,12))
+
+for ru, axo, axa, col in zip(allru, axs_o.flatten(), axs_a.flatten(), colors):
+    piuz = enso[ru]['tos'].groupby('time.year').mean()
+    years, gtas = glomeans[(ru, 'tas')]
+    # coef3, covmat3 = np.polyfit(years, gtas, deg = 3, cov = True)
+    # fitco3 = np.polyval(coef3, years)
+    g10 = ctl.butter_filter(gtas, 10)
+
+    #tasdetr = yeamean[(ru, 'tas')] - fitco3[:, np.newaxis, np.newaxis]
+    tasdetr = yeamean[(ru, 'tas')] - g10[:, np.newaxis, np.newaxis]
+    tasanom = tasdetr - tasdetr.mean('year')
+
+    #ctl.plot_map_contour(tasanom[0], central_lat_lon=(0, 205), draw_grid = True, add_rectangles=[160, 270, -5, 5])
+    tasanom_pac = tasanom.sel(lat = slice(-5, 5), lon = slice(160, 270)).mean('lat')
+
+    nino_yrs = piuz > 0.5
+    nina_yrs = piuz < -0.5
+    oknino = nino_yrs.values.flatten()
+    oknina = nina_yrs.values.flatten()
+
+    paclons = tasanom_pac.lon.values
+
+    ninodist = np.argmax(tasanom_pac[oknino].values, axis = 1)
+    ninadist = np.argmin(tasanom_pac[oknina].values, axis = 1)
+
+    axo.hist(paclons[ninodist], color = col)
+    axa.hist(paclons[ninadist], color = col)
+
+ctl.adjust_ax_scale(axs_o.flatten())
+ctl.adjust_ax_scale(axs_a.flatten())
+
+fig_o.savefig(cart_out + 'nino_lon_hist.pdf')
+fig_a.savefig(cart_out + 'nina_lon_hist.pdf')
