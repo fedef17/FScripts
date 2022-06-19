09a6c5c8f6ee365234f036b16cfa44299221bebb
Federico Fabiano <f.fabiano@isac.cnr.it>
2022-06-19 16:54:59 +0200

    bott oht

--- bottino_paper.py
+++ bottino_paper.py
@@ -699,9 +699,9 @@ for lev, tit in zip([700, 2000, 'deep'], ['0-700m', '700-2000m', '> 2000 m']):
         hatch.append(oht_patt[(ru, lev, 'pval')] < 0.05)
         subt.append(ru + ': ' + tit)
 
-[fig] = ctl.plot_multimap_contour(plpa, lats, lons, filename = carto + 'oht_patt_deep.pdf', subtitles = subt, plot_anomalies = False, cmap = 'viridis', figsize = (16,12), fix_subplots_shape = (3,3), cb_label = 'OHC trend (J/year)', add_hatching = hatch)
+[fig] = ctl.plot_multimap_contour(plpa, lats, lons, filename = carto + 'oht_patt_deep.pdf', subtitles = subt, plot_anomalies = False, cmap = 'viridis', figsize = (16,12), fix_subplots_shape = (3,3), cb_label = 'OHC trend (J/year)', add_hatching = hatch, hatch_styles = ['///', '', ''])
 
-for ax in fig.gca():
+for ax in fig.axes[:-1]:
     ax.set_facecolor('gainsboro')
 fig.savefig(carto + 'oht_patt_deep.pdf')
 
@@ -715,8 +715,8 @@ for lev, tit in zip([700, 2000, 'deep'], ['0-700m', '700-2000m', '> 2000 m']):
         hatch.append(oht_patt[(ru, lev, 'rel_pval')] < 0.05)
         subt.append(ru + ': ' + tit)
 
-[fig] = ctl.plot_multimap_contour(plpa, lats, lons, filename = carto + 'oht_patt_deep_rel.pdf', subtitles = subt, plot_anomalies = False, cmap = 'viridis', figsize = (16,12), fix_subplots_shape = (3,3), cb_label = 'OHC trend pattern')
-for ax in fig.gca():
+[fig] = ctl.plot_multimap_contour(plpa, lats, lons, filename = carto + 'oht_patt_deep_rel.pdf', subtitles = subt, plot_anomalies = False, cmap = 'viridis', figsize = (16,12), fix_subplots_shape = (3,3), cb_label = 'OHC trend pattern', add_hatching = hatch, hatch_styles = ['///', '', ''])
+for ax in fig.axes[:-1]:
     ax.set_facecolor('gainsboro')
 fig.savefig(carto + 'oht_patt_deep_rel.pdf')
 
@@ -729,6 +729,7 @@ for lev, tit in zip([700, 2000, 'deep'], ['0-700m', '700-2000m', '> 2000 m']):
         subt.append(ru + ': ' + tit)
 
 ctl.plot_multimap_contour(plpa, lats, lons, filename = carto + 'oht_patt_deep_change.pdf', subtitles = subt, plot_anomalies = False, cmap = ctl.heatmap(), figsize = (16,12), fix_subplots_shape = (3,3), cb_label = 'OHC change')
-for ax in fig.gca():
+
+for ax in fig.axes[:-1]:
     ax.set_facecolor('gainsboro')
 fig.savefig(carto + 'oht_patt_deep_change.pdf')
