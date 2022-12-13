c3efba10337caae634c8369b5fa6d1462f101a04
Federico Fabiano <f.fabiano@isac.cnr.it>
2022-12-13 16:01:12 +0100

    bott 1000 seaice

--- bottino_overview_seaice.py
+++ bottino_overview_seaice.py
@@ -88,46 +88,51 @@ allnams3 = allnams2 + ['stabilization-hist-1990']
 allru3 = allru2 + ['b990']
 colors3 = colors2 + ['teal']
 
+allruok = ['b990', 'b025', 'b050', 'b065', 'b080', 'b100']
+colok = ['lightslategray', 'forestgreen', 'orange', 'chocolate', 'maroon', 'violet']
+
+na = ''
+
 #for ru, col in zip(allruadd2, colorsadd2):
-# for na, ru, col in zip(allnams3, allru3, colors3):
-#     print(ru)
-#     mem = 'r1'
-#     if ru in ['ssp585', 'hist']: mem = 'r4'
-#
-#     if os.uname()[1] == 'hobbes':
-#         filna = '/nas/BOTTINO/CMIP6/LongRunMIP/EC-Earth-Consortium/EC-Earth3/{}/{}i1p1f1/{}/{}/*nc'
-#         filist = glob.glob(filna.format(na, mem, miptab, varnam))
-#     else:
-#         datadir = '/g100_scratch/userexternal/{}/ece3/{}/cmorized/'.format(user, ru)
-#         filna = datadir+'cmor_*/CMIP6/LongRunMIP/EC-Earth-Consortium/EC-Earth3/*/r1i1p1f1/{}/{}/g*/v*/{}*nc'
-#         filist = glob.glob(filna.format(miptab, varnam, varnam))
-#
-#     gigi = xr.open_mfdataset(filist, use_cftime=True)
-#
-#     try:
-#         lat = np.array(gigi.lat.data)
-#     except:
-#         print('lat name is latitude')
-#         lat = np.array(gigi.latitude.data)
-#
-#     seaice = np.array(gigi.siconc.data)
-#     #okslat = lat > 40.
-#     for ii, emi, okslat in zip([0,1], ['N', 'S'], [lat > 40, lat < -40]):
-#         areaok = areaT[okslat]
-#         oksi = seaice[:, okslat]
-#         oksi[oksi < 15.] = 0.
-#         oksi[oksi > 15.] = 1.
-#         oksiarea = oksi*areaok[np.newaxis, :]
-#         seaicearea = np.nansum(oksiarea, axis = 1)
-#
-#         dates = np.array(gigi.time.data)
-#         okmarch = np.array([da.month == 3 for da in dates])
-#         oksept = np.array([da.month == 9 for da in dates])
-#
-#         resdict[(ru, varnam, 'glomean', 'mar', emi)] = seaicearea[okmarch]
-#         resdict[(ru, varnam, 'glomean', 'sep', emi)] = seaicearea[oksept]
-#
-# pickle.dump(resdict, open(cart_out + 'seaicearea.p', 'wb'))
+for ru, col in zip(allruok, colok):
+    print(ru)
+    mem = 'r1'
+    if ru in ['ssp585', 'hist']: mem = 'r4'
+
+    if os.uname()[1] == 'hobbes':
+        filna = '/nas/BOTTINO/CMIP6/LongRunMIP/EC-Earth-Consortium/EC-Earth3/{}/{}i1p1f1/{}/{}/*nc'
+        filist = glob.glob(filna.format(na, mem, miptab, varnam))
+    else:
+        datadir = '/g100_scratch/userexternal/{}/ece3/{}/cmorized/'.format(user, ru)
+        filna = datadir+'cmor_*/CMIP6/LongRunMIP/EC-Earth-Consortium/EC-Earth3/*/r1i1p1f1/{}/{}/g*/v*/{}*nc'
+        filist = glob.glob(filna.format(miptab, varnam, varnam))
+
+    gigi = xr.open_mfdataset(filist, use_cftime=True)
+
+    try:
+        lat = np.array(gigi.lat.data)
+    except:
+        print('lat name is latitude')
+        lat = np.array(gigi.latitude.data)
+
+    seaice = np.array(gigi.siconc.data)
+    #okslat = lat > 40.
+    for ii, emi, okslat in zip([0,1], ['N', 'S'], [lat > 40, lat < -40]):
+        areaok = areaT[okslat]
+        oksi = seaice[:, okslat]
+        oksi[oksi < 15.] = 0.
+        oksi[oksi > 15.] = 1.
+        oksiarea = oksi*areaok[np.newaxis, :]
+        seaicearea = np.nansum(oksiarea, axis = 1)
+
+        dates = np.array(gigi.time.data)
+        okmarch = np.array([da.month == 3 for da in dates])
+        oksept = np.array([da.month == 9 for da in dates])
+
+        resdict[(ru, varnam, 'glomean', 'mar', emi)] = seaicearea[okmarch]
+        resdict[(ru, varnam, 'glomean', 'sep', emi)] = seaicearea[oksept]
+
+pickle.dump(resdict, open(cart_out + 'seaicearea_1000.p', 'wb'))
 
 
 for ru, col, (ye1, ye2) in zip(allruall, colall, okye):
@@ -152,7 +157,7 @@ axs[0,0].set_ylabel(r'Sea ice extent (m$^2$)')
 axs[1,0].set_ylabel(r'Sea ice extent (m$^2$)')
 #axs[1,1].legend()
 ctl.custom_legend(fig, colall, allruall, ncol = 5)
-fig.savefig(cart_out + 'bottseaice.pdf')
+fig.savefig(cart_out + 'bottseaice_1000.pdf')
 
 sys.exit()
 
