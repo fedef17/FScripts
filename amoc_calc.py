1b5e64fab343042a813e4ddb1f28f1fc7ef5969a
Federico Fabiano <f.fabiano@isac.cnr.it>
2023-03-06 17:37:10 +0100

    bott smoc

--- amoc_calc.py
+++ amoc_calc.py
@@ -91,10 +91,10 @@ for ru in allru:
 
         ### SMOC
         # amax = coso.sel(basin = 0, lev = slice(500., 3000.), rlat = slice(-90, -40)).mean('time').max(['rlat', 'lev']).values
-        amax = coso.sel(basin = 0, lev = slice(2000., None), rlat = slice(-90, -35)).mean('time').max(['rlat', 'lev']).values
+        amax = coso.sel(basin = 0, lev = slice(2000., None), rlat = slice(-90, -35)).mean('time').min(['rlat', 'lev']).values
         smoc_max.append(amax)
 
-        zuki = coso.sel(basin = 0, lev = slice(2000., None), rlat = slice(-90, -35)).mean('time').argmax(['rlat', 'lev'])
+        zuki = coso.sel(basin = 0, lev = slice(2000., None), rlat = slice(-90, -35)).mean('time').argmin(['rlat', 'lev'])
         maxlev = coso.sel(lev = slice(2000., None)).lev[zuki['lev']].values
         smoc_max_lev.append(maxlev)
 
